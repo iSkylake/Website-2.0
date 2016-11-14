510418bd700dcfb9f554ff1f2ed9802f18348179
Llorch <SkylakeBeta@gmail.com>
2016-11-13 21:20:02 -0800

    Add animate.css and delete console.log from smooth scroll function

--- public/javascript/javascript.js
+++ public/javascript/javascript.js
@@ -11,7 +11,6 @@ $('a[href*="#"]:not([href="#"])').click(function() {
   if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
     var target = $(this.hash);
     target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-    console.log(target);
     if (target.length) {
       $('html, body').animate({
         scrollTop: target.offset().top - 80
