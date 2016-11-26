201f80c7597c3c2380cc419d1e8f0e5e46665747
Llorch <SkylakeBeta@gmail.com>
2016-11-25 23:44:42 -0800

    Update bootstrap.js to newer version and move scrollspy to javascript

--- public/javascript/javascript.js
+++ public/javascript/javascript.js
@@ -7,18 +7,18 @@
 // $('#blog').removeAttr('href')
 
 // Smooth scroll
-$('a[href*="#"]:not([href="#"])').click(function() {
-  if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-    var target = $(this.hash);
-    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-    if (target.length) {
-      $('html, body').animate({
-        scrollTop: target.offset().top - 80
-      }, 700);
-      return false;
-    }
-  }
-});
+// $('a[href*="#"]:not([href="#"])').click(function() {
+//   if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+//     var target = $(this.hash);
+//     target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+//     if (target.length) {
+//       $('html, body').animate({
+//         scrollTop: target.offset().top - 80
+//       }, 700);
+//       return false;
+//     }
+//   }
+// });
 
 // function Scroll(){
 // var top = document.getElementById('header');
@@ -39,3 +39,30 @@ $('a[href*="#"]:not([href="#"])').click(function() {
 //   window.addEventListener("scroll",Scroll);
 
 // $('body').scrollspy({ target: '.navbar' })
+
+$(document).ready(function(){
+  // Add scrollspy to <body>
+  $('body').scrollspy({target: ".navbar", offset: 90});   
+
+  // Add smooth scrolling on all links inside the navbar
+  $("#myNavbar a").on('click', function(event) {
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top - 90
+      }, 800, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    }  // End if
+  });
+});
